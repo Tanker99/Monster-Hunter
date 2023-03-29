b43e3f6bb1b0c44eacc0887a899668628e2a8279
DerBringer <100408922+DerBringer@users.noreply.github.com>
2023-03-29 12:24:43 +0200

    Namen und Trank DB Update

--- src/main/java/Trank.java
+++ src/main/java/Trank.java
@@ -13,13 +13,13 @@ public class Trank {
     public String text;
 
     //Defin Variablen
-    public static final int tAnzahl = 8;
-    BufferedImage tBild[] = new BufferedImage[8];
+    public static final int tAnzahl = 5;
+    BufferedImage tBild[] = new BufferedImage[5];
 
     //Datenbank
-    public static String[] tName = {"Einhorn-Pipi","Rote Bete Saft","Pep-sie Gemisch","Ingwer-shot","Met","Rüstung6","Rüstung7","Rüstung8"};
-    public static int[] tKraft = {6,8,4,12,-4,6,7,8};
-    public static int[] tGoldwert = {10,4,2,7,9,6,7,8};
+    public static String[] tName = {"Einhorn-Pipi","Rote Bete Saft","Pep-sie Gemisch","Ingwer-shot","Met","Wasser"};
+    public static int[] tKraft = {6,8,4,12,-4,2};
+    public static int[] tGoldwert = {10,4,2,7,9,1};
     public static String[] tText = {"",""};
 
     public Trank(GamePanel gp){
@@ -49,14 +49,12 @@ public class Trank {
         this.gp = gp;
      
         try {
-            tBild[0] = ImageIO.read(Trank.class.getResource("/items/Schwacher Bogen.png"));
-            tBild[1] = ImageIO.read(Trank.class.getResource("/items/Starker Bogen.png"));
-            tBild[2] = ImageIO.read(Trank.class.getResource("/items/Axt.png"));
-            tBild[3] = ImageIO.read(Trank.class.getResource("/items/Streitaxt.png"));
-            tBild[4] = ImageIO.read(Trank.class.getResource("/items/Schwert.png"));
-            tBild[5] = ImageIO.read(Trank.class.getResource("/items/Feuerschwert.png"));
-            tBild[6] = ImageIO.read(Trank.class.getResource("/items/Morgenstern.png"));
-            tBild[7] = ImageIO.read(Trank.class.getResource("/items/Armbrust.png"));
+            tBild[0] = ImageIO.read(Trank.class.getResource("/items/Einhorn-Pipi.png"));
+            tBild[1] = ImageIO.read(Trank.class.getResource("/items/Rote Bete Saft.png"));
+            tBild[2] = ImageIO.read(Trank.class.getResource("/items/Pep-sie Gemisch.png"));
+            tBild[3] = ImageIO.read(Trank.class.getResource("/items/Ingwer-Shot.png"));
+            tBild[4] = ImageIO.read(Trank.class.getResource("/items/Met.png"));
+            tBild[5] = ImageIO.read(Trank.class.getResource("/items/Wasser.png"));
             
 
         } catch (IOException e) {
