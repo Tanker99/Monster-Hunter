eeac786a3d05755d2de0f88d568eb3e4a0f87cc4
DerBringer <100408922+DerBringer@users.noreply.github.com>
2023-03-29 12:18:36 +0200

    Upadte

--- src/main/java/Trank.java
+++ src/main/java/Trank.java
@@ -14,7 +14,7 @@ public class Trank {
 
     //Defin Variablen
     public static final int tAnzahl = 8;
-    BufferedImage trank[] = new BufferedImage[8];
+    BufferedImage tBild[] = new BufferedImage[8];
 
     //Datenbank
     public static String[] tName = {"Einhorn-Pipi","Rote Bete Saft","Pep-sie Gemisch","Ingwer-shot","Met","Rüstung6","Rüstung7","Rüstung8"};
@@ -45,4 +45,22 @@ public class Trank {
 
     }
 
+     public void getImage(){
+        this.gp = gp;
+     
+        try {
+            tBild[0] = ImageIO.read(Trank.class.getResource("/items/Schwacher Bogen.png"));
+            tBild[1] = ImageIO.read(Trank.class.getResource("/items/Starker Bogen.png"));
+            tBild[2] = ImageIO.read(Trank.class.getResource("/items/Axt.png"));
+            tBild[3] = ImageIO.read(Trank.class.getResource("/items/Streitaxt.png"));
+            tBild[4] = ImageIO.read(Trank.class.getResource("/items/Schwert.png"));
+            tBild[5] = ImageIO.read(Trank.class.getResource("/items/Feuerschwert.png"));
+            tBild[6] = ImageIO.read(Trank.class.getResource("/items/Morgenstern.png"));
+            tBild[7] = ImageIO.read(Trank.class.getResource("/items/Armbrust.png"));
+            
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("DB.Trank Image Error");
+     }
 }
