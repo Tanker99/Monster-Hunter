2ff4a4f4b01eb2b2246813dfe4d9688f4f841503
DerBringer <100408922+DerBringer@users.noreply.github.com>
2023-03-29 11:44:43 +0200

    Waffe Update

--- src/main/java/Waffe.java
+++ src/main/java/Waffe.java
@@ -3,65 +3,54 @@
 import java.awt.image.BufferedImage;
 
 public class Waffe {
+    //Standart
+    GamePanel gp;
 
-    public static final int Wanzahl = 0;
+    //Get Variablen
     public String name;
-    public int Kraft;
-    public int Goldwert;
-    BufferedImage waffe[] =new BufferedImage[7];
+    public int kraft;
+    public int goldwert;
+    public String text;
 
+    //Defin Variablen
+    public static final int Wanzahl = 7;
+    BufferedImage waffe[];
+
+    //Datenbank
     public static String[] Waffen = {"Schwacher Bogen", "Starker Bogen", "Streitaxt (Eine KLinge)", "Streitaxt (Doppelte Klinge)", "Schwert", "Feuerschwert", "Morgenstern", "Armbrust"};
     public static int[] WKraft = {2, 5, 4, 6, 3, 5, 6, 4};
     public static int[] WGoldwert = {3, 7, 4, 8, 4, 9, 6, 5};
 
-    public void getWaffe(int n) {
-        name = Waffe.Waffen[n];
-        Kraft = Waffe.WKraft[n];
-        Goldwert = Waffe.WGoldwert[n];
-    }
-/*
-    public void IMage(GamePanel gp){
+    public Waffe(GamePanel gp){
         this.gp = gp;
-        BufferedImage[] bufferedImages = new BufferedImage[gp.waffe.Wanzahl];
-        waffe[] = /new BufferedImage[gp.waffe.Wanzahl];
+        waffe[] = new BufferedImage[Wanzahl];
+        getImage();
         
-        try{
-        waffe[1] = ImageIO.read(waffe.class.getResource("main/resources/items/Feuerschwert.png"));
-        } 
-        catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("DB.Waffe Image Error");
-        }
-    
     }
-    public BufferedImage loadImage() throws IOException {
-        int i = 0;
-        BufferedImage test;
-        test = ImageIO.read(waffe.class.getResource("main/resources/items/Feuerschwert.png"));
-        switch (i){
-            case 1:
-                return test;
-        }
 
+    public void getWaffedetails(int n) {
+        name = Waffe.Wname[n];
+        kraft = Waffe.Wkraft[n];
+        goldwert = Waffe.Wgoldwert[n];
+    }
+
+    public void getImage(){
+        this.gp = gp;
         
         try {
-            waffe[0] = ImageIO.read(waffe.class.getResource("main/resources/items/Schwacher Bogen.png"));
-           waffe[1] = ImageIO.read(waffe.class.getResource("main/resources/items/Starker Bogen.png"));
-            waffe[2] = ImageIO.read(waffe.class.getResource("main/resources/items/Streitaxt (Eine KLinge).png"));
-            waffe[3] = ImageIO.read(waffe.class.getResource("main/resources/items/Streitaxt (Doppelte Klinge).png"));
-            waffe[4] = ImageIO.read(waffe.class.getResource("main/resources/items/Schwert.png"));
-            waffe[5] = ImageIO.read(waffe.class.getResource("main/resources/items/Feuerschwert.png"));
-            waffe[6] = ImageIO.read(waffe.class.getResource("main/resources/items/Morgenstern.png"));
-            waffe[7] = ImageIO.read(waffe.class.getResource("main/resources/items/Armbrust.png"));
+            waffe[0] = ImageIO.read(Waffe.class.getResource("main/resources/items/Schwacher Bogen.png"));
+            waffe[1] = ImageIO.read(Waffe.class.getResource("main/resources/items/Starker Bogen.png"));
+            waffe[2] = ImageIO.read(Waffe.class.getResource("main/resources/items/Streitaxt (Eine KLinge).png"));
+            waffe[3] = ImageIO.read(Waffe.class.getResource("main/resources/items/Streitaxt (Doppelte Klinge).png"));
+            waffe[4] = ImageIO.read(Waffe.class.getResource("main/resources/items/Schwert.png"));
+            waffe[5] = ImageIO.read(Waffe.class.getResource("main/resources/items/Feuerschwert.png"));
+            waffe[6] = ImageIO.read(Waffe.class.getResource("main/resources/items/Morgenstern.png"));
+            waffe[7] = ImageIO.read(Waffe.class.getResource("main/resources/items/Armbrust.png"));
             
 
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("DB.Waffe Image Error");
         }
-         
-        return test;
     }
-
- */
 }
