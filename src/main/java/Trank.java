a9e547d96b02707713a55945520177dc5e8d2309
DerBringer <100408922+DerBringer@users.noreply.github.com>
2023-03-29 11:57:10 +0200

    Korrektur

--- src/main/java/Trank.java
+++ src/main/java/Trank.java
@@ -14,15 +14,13 @@ public class Trank {
 
     //Defin Variablen
     public static final int tAnzahl = 8;
-    BufferedImage waffe[] = new BufferedImage[8];
+    BufferedImage trank[] = new BufferedImage[8];
 
-    public static final int Tanzahl = 0;
-    public String name;
-    public int Kraft;
-    public int Goldwert;
-    public static String[] Traenke={"Einhorn-Pipi","Rote Bete Saft","Pep-sie Gemisch","Ingwer-shot","Met","Rüstung6","Rüstung7","Rüstung8"};
-    public static int[] TKraft={6,8,4,12,-4,6,7,8};
-    public static int[] TGoldwert={10,4,2,7,9,6,7,8};
+    //Datenbank
+    public static String[] tName = {"Einhorn-Pipi","Rote Bete Saft","Pep-sie Gemisch","Ingwer-shot","Met","Rüstung6","Rüstung7","Rüstung8"};
+    public static int[] tKraft = {6,8,4,12,-4,6,7,8};
+    public static int[] tGoldwert = {10,4,2,7,9,6,7,8};
+    public static String[] tText = {"",""};
 
     public void getTrank(int n){
         name = Trank.Traenke[n];
