0de06ff67ce50e1ebdb48482bc8cb8936afd2b43
DerBringer <100408922+DerBringer@users.noreply.github.com>
2023-03-30 16:55:27 +0200

    Rüstung DB Update

--- src/main/java/Ruestung.java
+++ src/main/java/Ruestung.java
@@ -1,16 +1,64 @@
-public class Ruestung {
-    public static final int Ranzahl = 0;
-    public String name;
-    public int Kraft;
-    public int Goldwert;
-    public static String[] Ruestungen={"Rüstung1","Rüstung2","Rüstung3","Rüstung4","Rüstung5","Rüstung6","Rüstung7","Rüstung8"};
-    public static int[] RKraft={1,2,3,4,5,6,7,8};
-    public static int[] RGoldwert={1,2,3,4,5,6,7,8};
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
-    public void getRuestung(int n){
-        name = Ruestung.Ruestungen[n];
-        Kraft = Ruestung.RKraft[n];
-        Goldwert = Ruestung.RGoldwert[n];
+public class Ruestung {
+    //Standart
+    GamePanel gp;
+
+    //Get Variablen
+    public String name;
+    public int kraft;
+    public int goldwert;
+    public String text;
+
+    //Define Variablen
+    public static final int rAnzahl = 6;
+    BufferedImage rBild[] = new BufferedImage[6];
+    
+    //Datenbank
+    public static String[] rName={"Rüstung1","Rüstung2","Rüstung3","Rüstung4","Rüstung5","Rüstung6","Rüstung7","Rüstung8"};
+    public static int[] rKraft={1,2,3,4,5,6,7,8};
+    public static int[] rGoldwert={1,2,3,4,5,6,7,8};
+    public static String[] rText = {"",""}
+    
+    public Trank(GamePanel gp){
+        this.gp = gp;
+        getImage();
+        
     }
+
+    public String getName(int n){
+        return rName[n];
+    }
+
+    public int getKraft(int n){
+        return rKraft[n];
+    }
+
+    public int getGoldwert(int n){
+        return rGoldwert[n];
+    }
+
+    public String getText(int n){
+        return rText[n];
+    }
+    
+    public void getImage(){
+        this.gp = gp;
+     
+        try {
+            rBild[0] = ImageIO.read(Ruestung.class.getResource("/items/Einhorn-Pipi.png"));
+            rBild[1] = ImageIO.read(Ruestung.class.getResource("/items/Rote Bete Saft.png"));
+            rBild[2] = ImageIO.read(Ruestung.class.getResource("/items/Pep-sie Gemisch.png"));
+            rBild[3] = ImageIO.read(Ruestung.class.getResource("/items/Ingwer-Shot.png"));
+            rBild[4] = ImageIO.read(Ruestung.class.getResource("/items/Met.png"));
+            rBild[5] = ImageIO.read(Ruestung.class.getResource("/items/Wasser.png"));
+            
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("DB.Rüstung Image Error");
+     }
 }
 
