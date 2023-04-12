ca3577e9c7f825a13fc8ef9f80a6f445510a1994
maier <maierlars336@gmail.com>
2023-04-12 15:29:11 +0200

    ..

--- src/main/java/Fight.java
+++ src/main/java/Fight.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
@@ -9,20 +10,50 @@ public class Fight {
 
     KeyHandler keyH;
 
+    private JPanel slot[]=new JPanel[3];
 
-
-    public Fight(GamePanel gp,KeyHandler keyH) {
+    public Fight(GamePanel gp, KeyHandler keyH) {
         this.gp = gp;
         this.keyH = keyH;
     }
 
-    public void draw(Graphics2D g2){
+    public void draw(Graphics2D g2) {
         this.g2 = g2;
-        drawcha();
+        drawcharakter();
     }
 
-   public void drawcha(){
+    public void drawcharakter() {
 
-   }
+    }
 
+    public void drawButton(){
+        int x= (int)(gp.screenWidth/2);
+        int y= (int)(gp.screenHeight*0.3);
+        int width= 100;
+        int height= 100;
+
+        g2.drawRect(x,y,width,height);
+    }
+
+
+    /*public void kampf() {
+        for(int i=0;i<=lebenSpieler||i<=lebenMonster;i++){      //leben aus DB???
+            int kraftSpieler=  gp.player.kraft;     //aus DB???
+            int kraftMonster=
+        if()   {        //keyHandler???
+            lebenMonster-=(kraftSpieler);
+        }else{          //zeit nachdem Monster angreift?? oder nacheinenader???
+            lebenSpieler-=(kraftMonster);
+        }
+        if(lebenSpieler<=0){
+                                //in gp tot???
+        }
+        if(lebenMonster<=0){
+
+        }
+        }
+    }
+
+
+     */
 }
