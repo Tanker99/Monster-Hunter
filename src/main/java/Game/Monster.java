27b4c9d85a65099fd3574e051f042bf933d8f827
DerBringer <100408922+DerBringer@users.noreply.github.com>
2023-04-14 13:24:38 +0200

    anpassung der Werte

--- src/main/java/Game/Monster.java
+++ src/main/java/Game/Monster.java
@@ -19,9 +19,9 @@ public class Monster{
 
     //Datenbank
     public static String[] mName={"tollwütiges Schwein","hungriger Ork","Ghoul","Untoter Ritter"};
-    public static int[] mHealth={80,95,104,110,140};
-    public static int[] mDefense={5,9,12,16,20};
-    public static int[] mAttack={4,7,9,12,17};
+    public static int[] mHealth={80,95,104,125};
+    public static int[] mDefense={5,9,12,16,25};
+    public static int[] mAttack={4,7,9,12,18};
 
 
     public String getName(int n){
