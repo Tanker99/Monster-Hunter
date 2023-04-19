394756f3aa1d0e2f5dd7ce4460f64e1df4db8750
Tanker99 <lukas_hess@gmx.net>
2023-04-19 11:52:19 +0200

    .

--- src/main/java/Items/Ruestung.java
+++ src/main/java/Items/Ruestung.java
@@ -22,8 +22,8 @@ public class Ruestung extends Items{
     public static int[] rKraft={10,8,7,28,22,30};
     public static int[] rGoldwert={28,50,24,70,75,80};
     public static String[] rText = {
-    "Damals wie heute bietet eine R�stung aus hochwertigem Eisen einen guten Schutz, welcher dem Tr�ger im Kampf einen Vorteil verschafft.",
-    "Auch wenn die Weichheit des Materials im Kampf von Nachteil ist, dr�ckt man durch diese R�stung Erfolg und Mut aus.",
+    "Damals wie heute bietet eine R�stung aus hochwertigem Eisen einen guten Schutz, welcher dem Träger im Kampf einen Vorteil verschafft.",
+    "Auch wenn die Weichheit des Materials im Kampf von Nachteil ist, drückt man durch diese R�stung Erfolg und Mut aus.",
     "Weich aber robust. Leder ist in jedem Handwerk gefragt. Auch im Handwerk mit Schwertern!",
     "Dank seiner unfassbaren Robustheit bietet der Diamant auch im Kampf einen Schutz, der nahezu unzerstörbar ist!",
     "Sein grünes Schimmern könnte den Gegner blenden. Zu deinem Vorteil.",
