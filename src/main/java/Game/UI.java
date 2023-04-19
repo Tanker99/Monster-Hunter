f0f01ddea8b8588c5ae3fa838a0995f05ea0b3be
Tanker99 <lukas_hess@gmx.net>
2023-04-19 11:21:44 +0200

    .

--- src/main/java/Game/UI.java
+++ src/main/java/Game/UI.java
@@ -12,7 +12,7 @@ public class UI{
     Graphics2D g2;
 
     //States
-    public int menuState = 2;
+    public int menuState = 1;
     private int oldMenuState;
 
     final int loadingState = 1;
@@ -77,7 +77,7 @@ public class UI{
         g2.setColor(Color.RED);
         g2.drawString("Gesundheitswarnung: ",tX,tY - 100);
         g2.setColor(Color.WHITE);
-        String text = "Willkommen bei Monster-Hunter! Bevor du startest, m�chten wir dich darauf hinweisen, dass es wichtig ist, auf deine Gesundheit zu achten. Wir empfehlen, w�hrend jeder Spielstunde des Spielens regelm��ige eine Pause von 15 Minuten einzulegen, um eine �beranstrengung zu vermeiden. Wenn du k�rperliche Beschwerden wie M�digkeit oder Schmerzen versp�rst, solltest du das Spiel unterbrechen und dich ausruhen. Bitte sorge au�erdem daf�r, dass du in einer angenehmen Sitzposition sitzt und ausreichend beleuchtet bist, um deine Augen zu schonen. Vielen Dank f�r deine Aufmerksamkeit und viel Spa� beim Spielen!";
+        String text = "Willkommen bei Monster-Hunter! Bevor du startest, möchten wir dich darauf hinweisen, dass es wichtig ist, auf deine Gesundheit zu achten. Wir empfehlen, während jeder Spielstunde des Spielens regelmäßige eine Pause von 15 Minuten einzulegen, um eine Überanstrengung zu vermeiden. Wenn du körperliche Beschwerden wie Mödigkeit oder Schmerzen verspürst, solltest du das Spiel unterbrechen und dich ausruhen. Bitte sorge außerdem dafür, dass du in einer angenehmen Sitzposition sitzt und ausreichend beleuchtet bist, um deine Augen zu schonen. Vielen Dank für deine Aufmerksamkeit und viel Spaß beim Spielen!";
         Font font = new Font("Arial", Font.PLAIN, 25);
         g2.setFont(font);
         gp.text.drawTextBetweenBox(g2,text,tX,tY,twidght);
